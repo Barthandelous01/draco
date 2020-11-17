22981cde046df9c52b0bce112df0e00bafa7f789
Caleb D.S. Brzezinski <calebdsb@protonmail.com>
2020-11-17 09:37:48 -0500

    Improved the notes API. Standard plugins are under development!

--- plugins/notes.lisp
+++ plugins/notes.lisp
@@ -7,17 +7,22 @@
 
 (defun init (arglist)
   (loading:add-to-arglist arglist
-			  `("notes" ,#'main "open the notes file for the project")))
+			  `("notes" ,#'main "[ep] edit or display notes")))
 
-  (defparameter +notes-dir+ (merge-pathnames
-			     (make-pathname :directory '(:relative "notes"))
-			     globals:+config-folder+)
-    "The directory in draco that keeps the notefiles.")
+(defparameter +notes-dir+ (merge-pathnames
+			   (make-pathname :directory '(:relative "notes"))
+			   globals:+config-folder+)
+  "The directory in draco that keeps the notefiles.")
 
 (defparameter +notes-filetype+ "org"
   "The filetype of notes. Change me if you don't like org!")
 
-(defun main (_)
+(defun main (args)
+  (cond ((string= (first args) "e") (edit-notes))
+	((string= (first args) "p") (print-notes))
+	(t (format t "Unknown command: ~a" (first args)))))
+
+(defun edit-notes ()
   (let ((notefile (ensure-directories-exist (merge-pathnames
 					     (make-pathname
 					      :type +notes-filetype+
@@ -29,3 +34,15 @@
 				    :input :interactive
 				    :error-output :interactive)
       (error (e) (format t "error opening editor~&")))))
+
+(defun print-notes ()
+  (let ((notefile (ensure-directories-exist (merge-pathnames
+					     (make-pathname
+					      :type +notes-filetype+
+					      :name globals:*current-filename*)
+					     +notes-dir+))))
+    (handler-case (uiop:run-program (list "bat" (namestring notefile))
+				    :output :interactive
+				    :input :interactive
+				    :error-output :interactive)
+      (error (e) (format t "Unknown error: ~a~&" e)))))
